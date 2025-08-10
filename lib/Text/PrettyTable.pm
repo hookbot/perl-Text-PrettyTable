b16928b20a3b8d46b7eb106b7478fa322fb27e20
HookBot <hookbot@github.com>
2025-08-09 19:30:12 -0600

    Commit paul version asis.

--- lib/Text/PrettyTable.pm
+++ lib/Text/PrettyTable.pm
@@ -1,83 +1,322 @@
-package Text::PrettyTable;
-
-use 5.008000;
-use strict;
-use warnings;
-
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Text::PrettyTable ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.02';
-
-# Preloaded methods go here.
-
-1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
+package PrettyTable;
 
 =head1 NAME
 
-Text::PrettyTable - Perl extension for blah blah blah
+PrettyTable - Allow for auto-fixed-widith formatting of raw data
+
+$Id: PrettyTable.pm,v 1.11 2014/03/07 17:37:08 paul Exp $
+
+=cut
+
+use strict;
+use warnings;
+
+our @border = ('| ', ' | ', ' |', ' ',
+               '+-', '-+-', '-+', '-',
+               '+-', '-+-', '-+', '-',
+               '+-', '-+-', '-+', '-');
+our @borderu = ("│ ", " │ ", " │", ' ',
+                '┌─', '─┬─', '─┐', '─',
+                '├─', '─┼─', '─┤', '─',
+                '└─', '─┴─', '─┘', '─');
+our $unibox = 1;
+our $split  = 100;
+our $qr_escape = "[^ -~]";
+our @EXPORT = qw(pretty_table);
+
+sub import {
+    no strict 'refs';  my $cl = shift;  my $pkg = caller;  @_ = @{"${cl}::EXPORT"} if ! @_;
+    !/^(?:_|new)/ && defined &{"${cl}::${_}"} ? (*{"${pkg}::${_}"} = \&{"${cl}::${_}"}) : die "Cannot export $_ from $cl to $pkg.\n" for @_;
+}
+
+sub new {
+    my $class = shift;
+    return bless ref($_[0]) ? $_[0] : {@_}, $class;
+}
+
+sub pretty_table { __PACKAGE__->_tablify(@_) }
+sub plain_text { shift->_tablify(@_) }
+
+sub _tablify {
+    my ($self, $data, $args) = @_;
+    $self = $self->new($args || {}) if ! ref $self;
+    local $self->{'_level'} = 1 + ($self->{'_level'} || 0);
+
+    my $uni   = exists($self->{'unibox'}) ? $self->{'unibox'} : $unibox;
+    local $split = $self->{'split'}  || $split if $self->{'_level'} == 1;
+    local @border = ref($uni) ? @$uni : map {utf8::decode(my $c = $_); $c} @borderu
+        and !$uni or local $qr_escape = "[^ -~".join('', @border)."]" if $uni && $self->{'_level'} == 1;
+
+    my @bucket;
+    my @title;
+    my @max;
+    my @dir;
+    my $add = sub {
+        my ($cols, $bucket) = @_;
+        if (!ref($cols)) {
+            for my $chunk ($split ? map {/(.{$split}|.+)/g} split /\n/, $cols : split /\n/, $cols) {
+                push @$bucket, $chunk;
+            }
+            return;
+        }
+        my $i = @$bucket;
+        for my $j (0 .. $#$cols) {
+            my $_split = $split;
+            my $val = $cols->[$j];
+            if (! defined $val) {
+                $val = '(undef)';
+            } elsif (ref $val) {
+                if (UNIVERSAL::isa($val, 'SCALAR')) {
+                    $val = (defined(&JSON::true)  && JSON::true()  eq $val) ? '(true)'
+                         : (defined(&JSON::false) && JSON::false() eq $val) ? '(false)'
+                         : "\\\"$$val\"";
+                } else {
+                    chomp($val = $self->_tablify($val));
+                    $_split = 0 if $_split && $val =~ /^\Q$border[4]\E/ && $val =~ /\Q$border[14]\E$/;
+                }
+            }
+            $dir[$j] = 1 if $val =~ /\D/ && $bucket == \@bucket; # TODO - we could work on our alignment
+            my $I = $i;
+            for my $chunk ($_split ? map {/(.{$_split}|.+)/g} split /\n/, $val : split /\n/, $val) {
+                $chunk =~ s/($qr_escape)/sprintf "\\%03o", ord $1/eg;
+                $bucket->[$I++]->[$j] = $chunk;
+                $max[$j] = length($chunk) if !$max[$j] || $max[$j] < length($chunk);
+            }
+        }
+    };
+
+
+    if (UNIVERSAL::isa($data, 'HASH')) {
+        my $title = $self->{'title'};
+        $add->($title, \@title) if $title;
+        my @keys = @{ $self->{'sort'} || [sort {($a eq 'id') ? -1 : ($b eq 'id') ? 1 : $a cmp $b } keys %$data] };
+        $add->([$_, $data->{$_}], \@bucket) for @keys;
+        $add->(['(empty hash)'], \@bucket) if !@bucket;
+    } elsif (UNIVERSAL::isa($data, 'ARRAY')) {
+        my %title;
+        if ($data->[0] && ref($data->[0]) eq 'HASH' && !$self->{'collapse'}) {
+            @title{keys %$_} = () for grep {ref($_) eq 'HASH'} @$data; # find all uniques
+            my @keys = @{ $self->{'sort'} || [sort {($a eq 'id') ? -1 : ($b eq 'id') ? 1 : $a cmp $b } keys %title] };
+            $add->(\@keys, \@title);
+            foreach my $row (@$data) {
+                if (ref($row) ne 'HASH') {
+                    $add->($row, \@bucket);
+                    next;
+                }
+                $add->([@$row{@keys}], \@bucket);
+            }
+        } else {
+            my $title = $self->{'title'};
+            $add->($title, \@title) if $title;
+            $add->([$_], \@bucket) for @$data;
+            $add->(['(empty array)'], \@bucket) if !@bucket;
+        }
+    }
+
+    my $indent = $self->{'indent'} || '';
+    my $sep = "${indent}$border[8]".join($border[9], map {$border[11] x $_} @max)."$border[10]\n";
+    my $fmt = "${indent}$border[0]".join($border[1], map {'%'.($dir[$_] ? '-' : '').$max[$_].'s'} 0..$#max)."$border[2]\n";
+
+    if (!$self->{'collapse'} and my $cols = $self->{'auto_collapse'}) {
+        $cols = $ENV{'COLUMNS'} || eval { die if ! -t STDOUT; require Term::ReadKey; (Term::ReadKey::GetTerminalSize(\*STDOUT))[0] } || 80 if $cols eq '1';
+        if (length($sep) - 1 > $cols) {
+            local $self->{'collapse'} = 1;
+            local $self->{'_level'} if $self->{'_level'} == 1;
+            return $self->_tablify($data);
+        }
+    }
+
+    my $out = "";
+    $out .= "${indent}$border[4]".join($border[5], map {$border[7] x $_} @max)."$border[6]\n";
+    no warnings 'uninitialized'; # because of multiline
+    for my $buck (\@title, \@bucket) {
+        for my $row (@$buck) {
+            if (ref $row) {
+                $out .= sprintf($fmt, @$row);
+            } else {
+                $out .= sprintf("$border[0]%-*s$border[2]\n", length($sep) - (length($border[0])+length($border[2])+1), $row);
+            }
+        }
+        if ($buck == \@title) {
+            $out .= $sep if @title;
+        } else {
+            $out .= "${indent}$border[12]".join($border[13], map {$border[15] x $_} @max)."$border[14]\n";
+        }
+    }
+
+    utf8::encode($out) if $self->{'_level'} == 1;
+    return $out;
+}
+
+1;
+
+__END__
 
 =head1 SYNOPSIS
 
-  use Text::PrettyTable;
-  blah blah blah
+    perl -Ilib -e 'use PrettyTable; print PrettyTable->plain_text([qw(Hello Hey There)])'
+    ┌───────┐
+    │ Hello │
+    │ Hey   │
+    │ There │
+    └───────┘
 
-=head1 DESCRIPTION
+    perl -Ilib -e 'use PrettyTable; print PrettyTable->plain_text([qw(99 1000 2)])'
+    ┌──────┐
+    │   99 │
+    │ 1000 │
+    │    2 │
+    └──────┘
 
-Stub documentation for Text::PrettyTable, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+    perl -Ilib -e 'use PrettyTable; print PrettyTable->plain_text([qw(99 1000 2)], {title => ["Stuff"], unibox => 0})'
+    +-------+
+    | Stuff |
+    +-------+
+    |    99 |
+    |  1000 |
+    |     2 |
+    +-------+
 
-Blah blah blah.
+    perl -Ilib -e 'use PrettyTable; print PrettyTable->plain_text({id=>23,hi => "HI", cool => 1})'
+    ┌──────┬────┐
+    │ id   │ 23 │
+    │ cool │ 1  │
+    │ hi   │ HI │
+    └──────┴────┘
 
-=head2 EXPORT
+    perl -Ilib -e 'use PrettyTable; print PrettyTable->plain_text({id=>23,hi => "HI", cool => 1}, {title => [qw(Key Value)]})'
+    ┌──────┬───────┐
+    │ Key  │ Value │
+    ├──────┼───────┤
+    │ id   │ 23    │
+    │ cool │ 1     │
+    │ hi   │ HI    │
+    └──────┴───────┘
 
-None by default.
+    perl -Ilib -e 'use PrettyTable; print PrettyTable->plain_text([{id=>23,hi => "HI", cool => 1}, {id => 7,hi => "George", cool => "two"}])'
+    ┌────┬──────┬────────┐
+    │ id │ cool │ hi     │
+    ├────┼──────┼────────┤
+    │ 23 │ 1    │ HI     │
+    │  7 │ two  │ George │
+    └────┴──────┴────────┘
 
+    perl -Ilib -e 'use PrettyTable; print PrettyTable->plain_text({id => 23, hi => "HI", cool => [qw(a cee)]})'
+    ┌──────┬─────────┐
+    │ id   │ 23      │
+    │ cool │ ┌─────┐ │
+    │      │ │ a   │ │
+    │      │ │ cee │ │
+    │      │ └─────┘ │
+    │ hi   │ HI      │
+    └──────┴─────────┘
 
+    perl -Ilib -e 'use PrettyTable; print PrettyTable->plain_text([{id => 23, hi => "HI", cool => [qw(a cee)]}])'
+    ┌────┬─────────┬────┐
+    │ id │ cool    │ hi │
+    ├────┼─────────┼────┤
+    │ 23 │ ┌─────┐ │ HI │
+    │    │ │ a   │ │    │
+    │    │ │ cee │ │    │
+    │    │ └─────┘ │    │
+    └────┴─────────┴────┘
 
-=head1 SEE ALSO
+    perl -Ilib -e 'use PrettyTable; print PrettyTable->plain_text([{id=>23,hi => "HI", cool => 1}, "Wow", {hi => "Row1\nRow2", cool => 98, id=>""}])'
+    ┌────┬──────┬──────┐
+    │ id │ cool │ hi   │
+    ├────┼──────┼──────┤
+    │ 23 │    1 │ HI   │
+    │ Wow              │
+    │    │   98 │ Row1 │
+    │    │      │ Row2 │
+    └────┴──────┴──────┘
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+    perl -Ilib -e 'use PrettyTable; print PrettyTable->plain_text([({one => "A"x50, two => "B"x50})x2])'
+    ┌────────────────────────────────────────────────────┬────────────────────────────────────────────────────┐
+    │ one                                                │ two                                                │
+    ├────────────────────────────────────────────────────┼────────────────────────────────────────────────────┤
+    │ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA │ BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB │
+    │ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA │ BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB │
+    └────────────────────────────────────────────────────┴────────────────────────────────────────────────────┘
 
-If you have a mailing list set up for your module, mention it here.
+    perl -Ilib -e 'use PrettyTable; print PrettyTable->plain_text([({one => "A"x50, two => "B"x50})x2], {auto_collapse => 100})'
+    ┌──────────────────────────────────────────────────────────────┐
+    │ ┌─────┬────────────────────────────────────────────────────┐ │
+    │ │ one │ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA │ │
+    │ │ two │ BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB │ │
+    │ └─────┴────────────────────────────────────────────────────┘ │
+    │ ┌─────┬────────────────────────────────────────────────────┐ │
+    │ │ one │ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA │ │
+    │ │ two │ BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB │ │
+    │ └─────┴────────────────────────────────────────────────────┘ │
+    └──────────────────────────────────────────────────────────────┘
 
-If you have a web site set up for your module, mention it here.
+    # auto_collapse => 1 will try and determine columns from the terminal
 
-=head1 AUTHOR
+=head1 METHODS
 
-Rob Brown, E<lt>bbb@cpan.orgE<gt>
+=over 4
 
-=head1 COPYRIGHT AND LICENSE
+=item pretty_table
 
-Copyright (C) 2025 by Rob Brown
+Function.  Exported by default.  Calls PrettyTable->plain_text(@_).
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.34.1 or,
-at your option, any later version of Perl 5 you may have available.
+    print pretty_table([qw(one two three)]);
 
+    print pretty_table($data);
+
+=item plain_text
+
+Method.  Can be called as a class method or an object method.
+When called as a class method, it will first use args passed as a second
+parameter to instatiate an object.
+
+    my $pt = PrettyTable->new({auto_collapse => 100});
+    print $pt->plain_text($data);
+
+    # or
+
+    print PrettyTable->plain_text($data, {auto_collapse => 100});
+
+=back
+
+=head1 ARGUMENTS
+
+=over 4
+
+=item auto_collapse
+
+If set will try and automatically shrink the width based on the terminal width.
+
+=item sort
+
+A sort order for keys.  By default it sorts hashes by key.  Any key
+not in this sort order will not be present in the output.
+
+=item split
+
+Default is 100.  Length at which to split long lines.  Can also be set
+via $PrettyTable::split.
+
+=item title
+
+By default hashes do not have a title header.  You can pass in a
+two item arrayref to label the columns.
+
+   title => [qw(Key Value)],
+
+Arrays of non-hashrefs also do not have a title header.  You can
+pass in a title header for these as well.
+
+   title => [qw(My Array Column Headings)],
+
+=item unibox
+
+Now default true.  Enables unicode box borders.  Can be either a true
+value, or can be a 16 element array defining the border box (See the
+code for a sample of alternate boxes).  Can also be set via
+$PrettyTable::unibox.
+
+=back
 
 =cut
